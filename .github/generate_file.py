88ff0747248341160e6a25c96843e3699da1d4e6
sergeygazaryan <sergey.gazaryan@gmail.com>
2024-06-23 14:00:38 +0400

    Create generate_file.py

---
+++ .github/generate_file.py
@@ -0,0 +1,25 @@
+import sys
+from jinja2 import Environment, FileSystemLoader
+import os
+
+def generate_file(template_name, filename, schedule_interval):
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template(template_name)
+    
+    output = template.render(filename=filename, schedule_interval=schedule_interval)
+    
+    # Create output filename by replacing extension and adding '-dag.py'
+    base_filename = os.path.splitext(filename)[0]
+    output_filename = f'{base_filename}-dag.py'
+    
+    os.makedirs('generated_files', exist_ok=True)
+    with open(f'generated_files/{output_filename}', 'w') as f:
+        f.write(output)
+    
+    return output_filename
+
+if __name__ == "__main__":
+    template_name = sys.argv[1]
+    filename = sys.argv[2]
+    schedule_interval = sys.argv[3]
+    output_filename = generate_file(template_name,
